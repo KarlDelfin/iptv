d67d3f939208b4a8150e15900a05156f36ddb87c
KarlDelfin <kardelfin@proweaver.net>
2026-05-15 22:16:51 +0800

    update fallback https and http

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,8 @@ import channelLists from '../assets/js/channels.json'
 export default {
   data() {
     return {
-      defaultChannel: 'http://136.239.173.2:6610/001/2/ch00000090990000001087/manifest.mpd?AuthInfo=v87HD9rEhwHiAdYyrP20Tg5pgSMSITY%2FHYvvCWJRp%2BqHRrK8UUahwItHhKpXgPXKytokK1MIobcue1ImXa0ZEA%3D%3D&version=v1.0&BreakPoint=0&virtualDomain=001.live_hls.zte.com&programid=ch00000000000000001176&contentid=ch00000000000000001176&videoid=ch00000090990000001087&recommendtype=0&userid=1287297673984&boid=001&stbid=02%3A00%3A00%3A00%3A00%3A00&terminalflag=1&profilecode=&usersessionid=U4DB4Z0H2EXXXX&NeedJITP=1&JITPMediaType=DASH&JITPDRMType=NO',
+      defaultChannel:
+        'http://136.239.173.2:6610/001/2/ch00000090990000001087/manifest.mpd?AuthInfo=v87HD9rEhwHiAdYyrP20Tg5pgSMSITY%2FHYvvCWJRp%2BqHRrK8UUahwItHhKpXgPXKytokK1MIobcue1ImXa0ZEA%3D%3D&version=v1.0&BreakPoint=0&virtualDomain=001.live_hls.zte.com&programid=ch00000000000000001176&contentid=ch00000000000000001176&videoid=ch00000090990000001087&recommendtype=0&userid=1287297673984&boid=001&stbid=02%3A00%3A00%3A00%3A00%3A00&terminalflag=1&profilecode=&usersessionid=U4DB4Z0H2EXXXX&NeedJITP=1&JITPMediaType=DASH&JITPDRMType=NO',
       player: null,
       channels: channelLists,
     }
@@ -38,20 +39,31 @@ export default {
 
       const videoEl = document.createElement('video')
       videoEl.className = 'video-js vjs-default-skin'
+      videoEl.controls = true
       this.$refs.player_container.appendChild(videoEl)
+
+      const adaptiveLink = channelLink.replace(/^https?:/i, '')
 
       this.player = videojs(videoEl, {
         autoplay: true,
         fluid: true,
-        sources: [{ src: channelLink}],
+        sources: [{ src: adaptiveLink }],
       })
     },
-  }
+  },
 }
 </script>
 
 <style scoped>
-.container { display: flex; }
-.player_wrapper { width: 100%; max-width: 900px; }
-.player_con { width: 100%; max-width: 800px; }
+.container {
+  display: flex;
+}
+.player_wrapper {
+  width: 100%;
+  max-width: 900px;
+}
+.player_con {
+  width: 100%;
+  max-width: 800px;
+}
 </style>
